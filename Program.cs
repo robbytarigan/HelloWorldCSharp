c1d42f832c2653913822ad97eacdb77077f92468
Robby Tarigan <robbytarigan@hotmail.com>
2015-07-29 23:11:41 +0100

    Exception filters usage.

--- Program.cs
+++ Program.cs
@@ -13,15 +13,29 @@ namespace HelloWorldCSharp6
             var product = new Product("Car", "  Should be without space. right.  ");
 
             Console.WriteLine(product.GetLongDescription());
-            
-            var viewingDatesProduct = new Product() 
+
+            var viewingDatesProduct = new Product()
             {
                 [0] = new DateTime(2015, 7, 29),
                 [2] = new DateTime(2015, 8, 1),
                 Version = "1.0.1"
             };
-            
+
             Console.WriteLine($"Viewing dates for version {viewingDatesProduct.Version} are {viewingDatesProduct[0]:dd/MM/yyyy} and {viewingDatesProduct[2]:dd/MM/yyyy}.");
+
+            // Exception filters demo
+            try
+            {
+                throw new Exception("E2");
+            }
+            catch (Exception ex) when (ex.Message == "E1")
+            {
+                Console.WriteLine("caught E1");
+            }
+            catch (Exception ex) when (ex.Message == "E2")
+            {
+                Console.WriteLine("caught E2");
+            }
 
             Console.ReadLine();
         }
