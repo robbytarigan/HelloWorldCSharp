c4f398dae22f5e8e916e8b11b12324447c3e9d8a
Robby Tarigan <robbytarigan@hotmail.com>
2017-12-07 22:33:17 +0000

    Inferred tuple element names.

--- 7.1/Example/Program.cs
+++ 7.1/Example/Program.cs
@@ -8,6 +8,8 @@ namespace Example
         // Feature: Async Main
         static async Task<int> Main(string[] args)
         {
+            DefaultExample();
+            InferredTuppleExample();
             return await Delay(2);
         }
 
@@ -18,6 +20,22 @@ namespace Example
 
             Console.WriteLine($"Returning ...");
             return value;
+        }
+
+        private static void DefaultExample() {
+            // Feature: Default literal Expressions
+           Func<string, bool> expression = default;
+
+           Console.WriteLine(expression == null);
+        }
+
+        private static void InferredTuppleExample() {
+            int count = 5;
+            string label = "Colours used in the map";
+            // Feature: Inferred tupple element names
+            var pair = (count, label);
+
+            Console.WriteLine($"count = {pair.count}, label = {pair.label}");
         }
     }
 }
