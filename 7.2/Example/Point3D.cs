b04c4e3af8e7aa3e1fa4555cfc394584c48e0547
Robby Tarigan <robbytarigan@hotmail.com>
2017-12-07 23:01:48 +0000

    The in keyword specifies that you are passing the parameter by reference and the called method does not modify the value passed to it.

---
+++ 7.2/Example/Point3D.cs
@@ -0,0 +1,13 @@
+namespace Example {
+    public struct Point3D {
+        public double X;
+        public double Y;
+        public double Z;
+
+        public Point3D(double x, double y, double z) {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+}
---
+++ 7.2/Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point1 = new Point3D(2, 3, 5);
+            var point2 = new Point3D(10, 7, 21);
+
+            var distance = CalculateDistance(point1, point2);
+
+            Console.WriteLine($"Distance point 1 to 2 is {distance}");
+        }
+
+        // Feature in parameters
+        private static double CalculateDistance(in Point3D point1, in Point3D point2) {
+            double xDifference = point1.X - point2.X;
+            double yDifference = point1.Y - point2.Y;
+            double zDifference = point1.Z - point2.Z;
+
+            return Math.Sqrt(xDifference * xDifference + yDifference * yDifference + zDifference * zDifference);
+        }
+    }
+}
