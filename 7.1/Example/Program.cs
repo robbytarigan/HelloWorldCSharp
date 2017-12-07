d1442b2a3cec552f251ed3affb5919515da521e3
Robby Tarigan <robbytarigan@hotmail.com>
2017-12-07 22:15:59 +0000

    Async main

---
+++ 7.1/Example/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Program
+    {
+        // Feature: Async Main
+        static async Task<int> Main(string[] args)
+        {
+            return await Delay(2);
+        }
+
+        private static async Task<int> Delay(int value) {
+            Console.WriteLine($"Delay for {value} seconds ...");
+
+            await Task.Delay(value * 1000);
+
+            Console.WriteLine($"Returning ...");
+            return value;
+        }
+    }
+}
