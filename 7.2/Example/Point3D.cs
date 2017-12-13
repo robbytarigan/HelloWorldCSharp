c86c8f768394d261ed4bdcdb3c2990f530ffde96
Robby Tarigan <robbytarigan@hotmail.com>
2017-12-13 22:12:43 +0000

    readonly struct type

--- 7.2/Example/Point3D.cs
+++ 7.2/Example/Point3D.cs
@@ -1,21 +1,17 @@
 namespace Example {
-    public struct Point3D {
-	/*
-	 * It's likely that the library using Point3D would often use the origin throughout the code. 
-	 * Every instance creates a new object on the stack. It may be advantageous to create a constant and return it by reference. 
-	 * But, if you return a reference to internal storage, you may want to enforce that the caller cannot modify the referenced storage.
-	 */
-	private static Point3D origin = new Point3D();
-	public static ref readonly Point3D Origin => ref origin;
+    public readonly struct ReadonlyPoint3D {
 
-        public double X;
-        public double Y;
-        public double Z;
-
-        public Point3D(double x, double y, double z) {
+        public ReadonlyPoint3D(double x, double y, double z) {
             X = x;
             Y = y;
             Z = z;
         }
+
+        public double X { get; }
+        public double Y { get; }
+        public double Z { get; }
+
+	private static readonly ReadonlyPoint3D origin = new ReadonlyPoint3D();
+	public static ref readonly ReadonlyPoint3D Origin => ref origin;
     }
 }
--- 7.2/Example/Program.cs
+++ 7.2/Example/Program.cs
@@ -16,6 +16,12 @@ namespace Example
 
 	    distance = CalculateDistance(point1, new Point3D());
 	    Console.WriteLine($"Distance point 1 to origin is {distance}");
+
+	    var readPoint1 = new ReadonlyPoint3D(2, 3, 5);
+	    var readPoint2 = new ReadonlyPoint3D(10, 7, 21);
+
+	    distance = CalculateDistance(readPoint1, readPoint2);
+	    Console.WriteLine("Distance between readonly point 1 to 2 is {0}", distance);
         }
 
         // Feature in parameters
@@ -29,5 +35,13 @@ namespace Example
 
             return Math.Sqrt(xDifference * xDifference + yDifference * yDifference + zDifference * zDifference);
         }
-    }
+
+         private static double CalculateDistance(in ReadonlyPoint3D point1, in ReadonlyPoint3D point2 = default) {
+            double xDifference = point1.X - point2.X;
+            double yDifference = point1.Y - point2.Y;
+            double zDifference = point1.Z - point2.Z;
+
+            return Math.Sqrt(xDifference * xDifference + yDifference * yDifference + zDifference * zDifference);
+        }
+   }
 }
