afa1128c99034fa62639646d5fd7cf773c6c2418
Robby Tarigan <robbytarigan@hotmail.com>
2018-05-17 22:42:13 +0100

    ref locals and returns example.

---
+++ 7.2/RefLocals/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RefLocals
+{
+    class Program
+    {
+        private static int[] _array =  { 1, 42, 23, 5 };
+
+        public static ref int ArrayValue(int index)
+        {
+            return ref _array[index];
+        }
+
+        static void Main()
+        {
+            ref var val = ref ArrayValue(1);
+            val++;
+            // Print: 1,43,23,5
+            Console.WriteLine(string.Join(",", _array));
+        }
+    }
+}
