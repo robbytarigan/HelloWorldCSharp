ad2e1538a5ad382b4ee885fa997944f766758985
Robby Tarigan <robbytarigan@hotmail.com>
2018-05-17 22:17:18 +0100

    Standalone discards example.

---
+++ 7.2/Discards/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Discards
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            ExecuteAsyncMethods().Wait();
+        }
+
+        private static async Task ExecuteAsyncMethods()
+        {
+            Console.WriteLine("About to launch a task...");
+            _ = Task.Run(() =>
+            {
+                var iterations = 0;
+                for (int ctr = 0; ctr < int.MaxValue; ctr++)
+                    iterations++;
+                Console.WriteLine("Completed looping operation...");
+                throw new InvalidOperationException();
+            });
+            await Task.Delay(7000);
+            Console.WriteLine("Exiting after 7 second delay");
+        }
+
+    }
+}
