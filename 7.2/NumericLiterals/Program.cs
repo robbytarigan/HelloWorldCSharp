e1f7465688fb4fc429e3b9373d7dd87d0c5e2237
Robby Tarigan <robbytarigan@hotmail.com>
2018-05-18 22:22:47 +0100

    C# 7.2: Leading underscores in numeric literals

---
+++ 7.2/NumericLiterals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NumericLiterals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var byteValue1 = 0b1111_0000;
+            
+            // Underscore can be added as first character
+            var byteValue2 = 0b_1111_0000;
+            Console.WriteLine("Number1 = {0}, Number2 = {1}", byteValue1, byteValue2);
+        }
+    }
+}
