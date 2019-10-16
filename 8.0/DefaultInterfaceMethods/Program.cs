47879c61b173aed35d9c3f7542002f64cc00531e
Robby Tarigan <robby.tarigan@cirium.com>
2019-10-16 16:01:45 +0000

    Initialise default interface methods project.

---
+++ 8.0/DefaultInterfaceMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefaultInterfaceMethods
+{
+    public interface ICustomer
+    {
+        IEnumerable<IOrder> PreviousOrders { get; }
+
+        DateTime DateJoined { get; }
+        DateTime? LastOrder { get; }
+        string Name { get; }
+        IDictionary<DateTime, string> Reminders { get; }
+    }
+
+    public interface IOrder
+    {
+        DateTime Purchased { get; }
+        decimal Cost { get; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
