7d2e6387d830b19305fa4efafdad83e156aaa500
Robby Tarigan <robby.tarigan@cirium.com>
2019-10-18 14:07:38 +0000

    Property pattern matching example. Refactoring the patterns into classes.

--- 8.0/PatternMatching/Program.cs
+++ 8.0/PatternMatching/Program.cs
@@ -2,74 +2,13 @@
 
 namespace PatternMatching
 {
-    public enum Rainbow
-    {
-        Red,
-        Orange,
-        Yellow,
-        Green,
-        Blue,
-        Indigo,
-        Violet
-    }
-
-    public class RGBColor
-    {
-        public RGBColor(int red, int green, int blue)
-        {
-            Red = red;
-            Green = green;
-            Blue = blue;
-        }
-
-        public int Red { get; }
-        public int Green { get; }
-        public int Blue { get; }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Switch Pattern Matching Example {0}", FromRainbow(Rainbow.Violet));
-        }
+            Console.WriteLine("Switch Expression Example {0}", SwitchExpression.FromRainbow(Rainbow.Violet));
 
-        public static RGBColor FromRainbow(Rainbow colorBand) =>
-            colorBand switch
-            {
-                Rainbow.Red => new RGBColor(0xFF, 0x00, 0x00),
-                Rainbow.Orange => new RGBColor(0xFF, 0x7F, 0x00),
-                Rainbow.Yellow => new RGBColor(0xFF, 0xFF, 0x00),
-                Rainbow.Green => new RGBColor(0x00, 0xFF, 0x00),
-                Rainbow.Blue => new RGBColor(0x00, 0x00, 0xFF),
-                Rainbow.Indigo => new RGBColor(0x4B, 0x00, 0x82),
-                Rainbow.Violet => new RGBColor(0x94, 0x00, 0xD3),
-                _ => throw new ArgumentException(message: "invalid enum value", paramName: nameof(colorBand)),
-            };
-
-        public static RGBColor FromRainbowClassic(Rainbow colorBand)
-        {
-            switch (colorBand)
-            {
-                case Rainbow.Red:
-                    return new RGBColor(0xFF, 0x00, 0x00);
-                case Rainbow.Orange:
-                    return new RGBColor(0xFF, 0x7F, 0x00);
-                case Rainbow.Yellow:
-                    return new RGBColor(0xFF, 0xFF, 0x00);
-                case Rainbow.Green:
-                    return new RGBColor(0x00, 0xFF, 0x00);
-                case Rainbow.Blue:
-                    return new RGBColor(0x00, 0x00, 0xFF);
-                case Rainbow.Indigo:
-                    return new RGBColor(0x4B, 0x00, 0x82);
-                case Rainbow.Violet:
-                    return new RGBColor(0x94, 0x00, 0xD3);
-                default:
-                    throw new ArgumentException(message: "invalid enum value", paramName: nameof(colorBand));
-            };
-        }
+            Console.WriteLine("Property Pattern Example {0}", PropertyPattern.ComputeSalesTax(new Address { State = "MI"}, 2M));
+        }       
     }
-
-
 }
---
+++ 8.0/PatternMatching/PropertyPattern.cs
@@ -0,0 +1,22 @@
+namespace PatternMatching
+{
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string State { get; set; }
+    }
+
+    public static class PropertyPattern
+    {
+        public static decimal ComputeSalesTax(Address location, decimal salePrice) =>
+                    location switch
+                    {
+                        { State: "WA" } => salePrice * 0.06M,
+                        { State: "MN" } => salePrice * 0.75M,
+                        { State: "MI" } => salePrice * 0.05M,
+                    // other cases removed for brevity...
+                    _ => 0M
+                    };
+    }
+}
