ac2ce7f79d3b999d3802abc9ee34a79415eb2721
Robby Tarigan <robby.tarigan@cirium.com>
2019-10-18 14:12:01 +0000

    Tupple pattern matching example.

--- 8.0/PatternMatching/Program.cs
+++ 8.0/PatternMatching/Program.cs
@@ -9,6 +9,8 @@ namespace PatternMatching
             Console.WriteLine("Switch Expression Example {0}", SwitchExpression.FromRainbow(Rainbow.Violet));
 
             Console.WriteLine("Property Pattern Example {0}", PropertyPattern.ComputeSalesTax(new Address { State = "MI"}, 2M));
+
+            Console.WriteLine("Tupple Pattern Example {0}", TupplePattern.RockPaperScissors("scissors", "paper"));
         }       
     }
 }
