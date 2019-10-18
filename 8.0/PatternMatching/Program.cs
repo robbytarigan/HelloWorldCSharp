ec7b195ffc0a25d79deccb7faa2841580c4d9702
Robby Tarigan <robby.tarigan@cirium.com>
2019-10-18 14:17:27 +0000

    Positional pattern matching example.

--- 8.0/PatternMatching/Program.cs
+++ 8.0/PatternMatching/Program.cs
@@ -11,6 +11,8 @@ namespace PatternMatching
             Console.WriteLine("Property Pattern Example {0}", PropertyPattern.ComputeSalesTax(new Address { State = "MI"}, 2M));
 
             Console.WriteLine("Tupple Pattern Example {0}", TupplePattern.RockPaperScissors("scissors", "paper"));
+
+            Console.WriteLine("Positional Pattern Example {0}", PositionalPattern.GetQuadrant(new Point(4, -3)));
         }       
     }
 }
