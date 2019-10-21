a8b2deb380a7909f72136f368495f36307fd55a9
Robby Tarigan <robbytarigan@hotmail.com>
2019-10-21 21:48:38 +0100

    Enable a ref struct to be disposed with accessible `Dispose` method.

---
+++ 8.0/DisposableRefStruct/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DisposableRefStruct
+{
+    class Program
+    {
+        ref struct Book
+        {
+            private readonly int id;
+
+            public Book(int id)
+            {
+                this.id = id;
+            }
+
+            public void Dispose()
+            {
+                Console.WriteLine("Dispose Book {0}", id);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            using var book = new Book(100);
+        }
+    }
+}
