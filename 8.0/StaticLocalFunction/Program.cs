1341406e1dd2fab97ae3b08e54b2e9bb893d5d07
Robby Tarigan <robbytarigan@hotmail.com>
2019-10-21 21:37:59 +0100

    You can now add the static modifier to local functions to ensure that local function doesn't capture (reference) any variables from the enclosing scope.

---
+++ 8.0/StaticLocalFunction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StaticLocalFunction
+{
+    class Program
+    {
+        private static int NonStaticLocal()
+        {
+            int y;
+            LocalFunction();
+            return y;
+
+            void LocalFunction() => y = 0;
+        }
+
+        private static int StaticLocal()
+        {
+            int y = 5;
+            int x = 7;
+            return Add(x, y);
+
+            static int Add(int left, int right) => left + right;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Non Static Local Function call {0}", NonStaticLocal());
+            Console.WriteLine("Static Local Function call {0}", StaticLocal());
+        }
+    }
+}
