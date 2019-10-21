cd348922bbefb5bbba3a7842011521de4da5d525
Robby Tarigan <robbytarigan@hotmail.com>
2019-10-21 21:31:27 +0100

    A using declaration is a variable declaration preceded by the using keyword. It tells the compiler that the variable being declared should be disposed at the end of the enclosing scope.

---
+++ 8.0/UsingDeclaration/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UsingDeclaration
+{
+    class Program
+    {
+        static int WriteLinesToFile(IEnumerable<string> lines)
+        {
+            using var file = new System.IO.StreamWriter("WriteLines2.txt");
+            // Notice how we declare skippedLines after the using statement.
+            int skippedLines = 0;
+            foreach (string line in lines)
+            {
+                if (!line.Contains("Second"))
+                {
+                    file.WriteLine(line);
+                }
+                else
+                {
+                    skippedLines++;
+                }
+            }
+            // Notice how skippedLines is in scope here.
+            return skippedLines;
+            // file is disposed here
+        }
+
+        static void Main(string[] args)
+        {
+            WriteLinesToFile(new[] { "Naruto", "Uzumaki", "Boruto", "Hinata", "Mawari", "Second", "Seventh"});
+
+            Console.WriteLine("Write to WriteLines2.txt");
+        }
+    }
+}
