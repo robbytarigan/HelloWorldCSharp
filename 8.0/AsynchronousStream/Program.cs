22d372c45564d6fb929f53df19af735770a34149
Robby Tarigan <robbytarigan@hotmail.com>
2019-10-24 21:53:15 +0100

    Asynchronous stream example.  Generates a sequence from 0 to 19, waiting 100 ms between generating each number.

---
+++ 8.0/AsynchronousStream/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsynchronousStream
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await foreach (var number in GenerateSequence())
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public static async System.Collections.Generic.IAsyncEnumerable<int> GenerateSequence()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                await Task.Delay(500);
+                yield return i;
+            }
+        }
+    }
+}
