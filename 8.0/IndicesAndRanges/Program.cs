35ca39eda322c21998c266835970a7b30ab320c5
Robby Tarigan <robbytarigan@hotmail.com>
2019-10-28 21:20:48 +0000

    Using variables

--- 8.0/IndicesAndRanges/Program.cs
+++ 8.0/IndicesAndRanges/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace IndicesAndRanges
 {
@@ -44,6 +45,43 @@ namespace IndicesAndRanges
             foreach (var word in lastPhrase)
                 Console.Write($"< {word} >");
             Console.WriteLine();
+
+            Index the = ^3;
+            Console.WriteLine(words[the]);
+            Range phrase = 1..4;
+            string[] text = words[phrase];
+            foreach (var word in text)
+                Console.Write($"< {word} >");
+            Console.WriteLine();
+
+            SpanExample();
+        }
+
+        private static void SpanExample() {
+            int[] numbers = Enumerable.Range(0, 100).ToArray();
+            int x = 12;
+            int y = 25;
+            int z = 36;
+
+            Console.WriteLine($"{numbers[^x]} is the same as {numbers[numbers.Length - x]}");
+            Console.WriteLine($"{numbers[x..y].Length} is the same as {y - x}");
+
+            Console.WriteLine("numbers[x..y] and numbers[y..z] are consecutive and disjoint:");
+            Span<int> x_y = numbers[x..y];
+            Span<int> y_z = numbers[y..z];
+            Console.WriteLine($"\tnumbers[x..y] is {x_y[0]} through {x_y[^1]}, numbers[y..z] is {y_z[0]} through {y_z[^1]}");
+
+            Console.WriteLine("numbers[x..^x] removes x elements at each end:");
+            Span<int> x_x = numbers[x..^x];
+            Console.WriteLine($"\tnumbers[x..^x] starts with {x_x[0]} and ends with {x_x[^1]}");
+
+            Console.WriteLine("numbers[..x] means numbers[0..x] and numbers[x..] means numbers[x..^0]");
+            Span<int> start_x = numbers[..x];
+            Span<int> zero_x = numbers[0..x];
+            Console.WriteLine($"\t{start_x[0]}..{start_x[^1]} is the same as {zero_x[0]}..{zero_x[^1]}");
+            Span<int> z_end = numbers[z..];
+            Span<int> z_zero = numbers[z..^0];
+            Console.WriteLine($"\t{z_end[0]}..{z_end[^1]} is the same as {z_zero[0]}..{z_zero[^1]}");
         }
     }
 }
