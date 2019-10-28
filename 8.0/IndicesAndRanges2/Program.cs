0d50cf194c57acbfb5bdbb60966b2200d677f052
Robby Tarigan <robbytarigan@hotmail.com>
2019-10-28 22:03:56 +0000

    Clearer way to use sub range. Enumerates just that range when calculating the min, max, and average

---
+++ 8.0/IndicesAndRanges2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace IndicesAndRanges2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] sequence = Sequence(1000);
+
+
+            for (int start = 0; start < sequence.Length; start += 100)
+            {
+                Range r = start..(start + 10);
+                var (min, max, average) = MovingAverage(sequence, r);
+                Console.WriteLine($"From {r.Start} to {r.End}:    \tMin: {min},\tMax: {max},\tAverage: {average}");
+            }
+
+            for (int start = 0; start < sequence.Length; start += 100)
+            {
+                Range r = ^(start + 10)..^start;
+                var (min, max, average) = MovingAverage(sequence, r);
+                Console.WriteLine($"From {r.Start} to {r.End}:  \tMin: {min},\tMax: {max},\tAverage: {average}");
+            }
+
+            (int min, int max, double average) MovingAverage(int[] subSequence, Range range) =>
+                (
+                    subSequence[range].Min(),
+                    subSequence[range].Max(),
+                    subSequence[range].Average()
+                );
+
+            int[] Sequence(int count) =>
+                Enumerable.Range(0, count).Select(x => (int)(Math.Sqrt(x) * 100)).ToArray();;
+        }
+    }
+}
