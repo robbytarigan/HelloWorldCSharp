1a42b402ccac3fffbcab428c7251d856c8668417
Robby Tarigan <robbytarigan@hotmail.com>
2019-10-28 21:11:44 +0000

    Examples of using indices and ranges.

---
+++ 8.0/IndicesAndRanges/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace IndicesAndRanges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = new string[]
+            {
+                            // index from start    index from end
+                "The",      // 0                   ^9
+                "quick",    // 1                   ^8
+                "brown",    // 2                   ^7
+                "fox",      // 3                   ^6
+                "jumped",   // 4                   ^5
+                "over",     // 5                   ^4
+                "the",      // 6                   ^3
+                "lazy",     // 7                   ^2
+                "dog"       // 8                   ^1
+            };              // 9 (or words.Length) ^0
+
+            Console.WriteLine($"The last word is {words[^1]}");
+            
+            string[] quickBrownFox = words[1..4];
+            foreach (var word in quickBrownFox)
+                Console.Write($"< {word} >");
+            Console.WriteLine();
+
+            string[] lazyDog = words[^2..^0];
+            foreach (var word in lazyDog)
+                Console.Write($"< {word} >");
+            Console.WriteLine();
+
+            string[] allWords = words[..]; // contains "The" through "dog".
+            string[] firstPhrase = words[..4]; // contains "The" through "fox"
+            string[] lastPhrase = words[6..]; // contains "the, "lazy" and "dog"
+            foreach (var word in allWords)
+                Console.Write($"< {word} >");
+            Console.WriteLine();
+            foreach (var word in firstPhrase)
+                Console.Write($"< {word} >");
+            Console.WriteLine();
+            foreach (var word in lastPhrase)
+                Console.Write($"< {word} >");
+            Console.WriteLine();
+        }
+    }
+}
