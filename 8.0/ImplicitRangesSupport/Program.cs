6ed5c2f4a2cfe4efe4f72d7714d8cc65c704a2c8
Robby Tarigan <robbytarigan@hotmail.com>
2019-10-28 21:53:31 +0000

    Implicit Ranges Support through `Length` property and `Slice` method which accept two int parameters.

---
+++ 8.0/ImplicitRangesSupport/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ImplicitRangesSupport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var effect = new SideEffect();
+            effect.Use();
+            effect.Use2();
+        }
+
+    }
+    class Collection
+    {
+        private int[] _array = new[] { 1, 2, 3 };
+
+        public int Length
+        {
+            get
+            {
+                Console.Write("Length ");
+                return _array.Length;
+            }
+        }
+
+        public int[] Slice(int start, int length)
+        {
+            var slice = new int[length];
+            Array.Copy(_array, start, slice, 0, length);
+            return slice;
+        }
+    }
+
+    class SideEffect
+    {
+        public Collection Get()
+        {
+            Console.Write("Get ");
+            return new Collection();
+        }
+
+        public void Use()
+        {
+            var array = Get()[0..2];
+            Console.WriteLine(array.Length);
+        }
+
+        public void Use2()
+        {
+            var array = Get()[..];
+            Console.WriteLine(array.Length);
+        }
+    }
+}
