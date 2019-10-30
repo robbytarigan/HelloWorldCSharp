6defde45d9d2633c8521a85014432a826543dad7
Robby Tarigan <robby.tarigan@cirium.com>
2019-10-30 11:01:43 +0000

    Use the `??=` operator to assign the value of its right-hand operand to its left-hand operand only if the left-hand operand evaluates to null.

---
+++ 8.0/NullCoalescingAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NullCoalescingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = null;
+            int? i = null;
+
+            numbers ??= new List<int>();
+            numbers.Add(i ??= 17);
+            numbers.Add(i ??= 20);
+
+            Console.WriteLine(string.Join(" ", numbers));  // output: 17 17
+            Console.WriteLine(i);  // output: 17
+        }
+    }
+}
