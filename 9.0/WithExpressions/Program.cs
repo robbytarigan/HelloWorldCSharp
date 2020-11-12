6df206392aec3703e011abb77f966a810617128f
Robby Tarigan <robby.tarigan@cirium.com>
2020-11-12 22:23:03 +0000

    Value-based equality

--- 9.0/WithExpressions/Program.cs
+++ 9.0/WithExpressions/Program.cs
@@ -11,6 +11,10 @@ namespace WithExpressions
 
             Console.WriteLine($"person is {person}");
             Console.WriteLine($"otherPerson is {otherPerson}");
+
+            var originalPerson = otherPerson with { LastName = "Nielsen" };
+            Console.WriteLine($"ReferenceEquals(person, originalPerson) = {object.ReferenceEquals(person, originalPerson)}");
+            Console.WriteLine($"Equals(person, originalPerson) = {object.Equals(person, originalPerson)}");
         }
 
         public record Person(string FirstName, string LastName);
