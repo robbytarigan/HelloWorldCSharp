439a1ddc38724a8fb993455f4cf843c449761670
Robby Tarigan <robby.tarigan@cirium.com>
2020-11-12 22:03:42 +0000

    Init only properties

---
+++ 9.0/InitOnlyProperties/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InitOnlyProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person { FirstName = "Mads", LastName = "Nielsen" }; // OK
+            // person.LastName = "Torgersen"; // ERROR!
+        }
+    }
+
+    public class Person
+    {
+        public string? FirstName { get; init; }
+        public string? LastName { get; init; }
+    }
+}
