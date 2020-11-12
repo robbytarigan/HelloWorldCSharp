8f74a20db00632c6bbc994c5b650275b4bafea41
Robby Tarigan <robby.tarigan@cirium.com>
2020-11-12 22:08:44 +0000

    Because init accessors can only be called during initialization, they are allowed to mutate readonly fields of the enclosing class, just like you can in a constructor.

---
+++ 9.0/InitAccessorsAndReadonlyFields/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InitAccessorsAndReadonlyFields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person { FirstName = "Robby", LastName = "Tarigan" };
+
+            Console.WriteLine($"{person.FirstName} {person.LastName}");
+
+            // person.FirstName = "Mads"; This is error
+        }
+
+        public class Person
+        {
+            private readonly string firstName = "<unknown>";
+            private readonly string lastName = "<unknown>";
+
+            public string FirstName
+            {
+                get => firstName;
+                init => firstName = (value ?? throw new ArgumentNullException(nameof(FirstName)));
+            }
+            public string LastName
+            {
+                get => lastName;
+                init => lastName = (value ?? throw new ArgumentNullException(nameof(LastName)));
+            }
+        }
+    }
+}
