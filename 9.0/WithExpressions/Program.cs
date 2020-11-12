2593f448c68a1087aa0e820cb589992e966427af
Robby Tarigan <robby.tarigan@cirium.com>
2020-11-12 22:19:46 +0000

    The with-expression works by actually copying the full state of the old object into a new one, then mutating it according to the object initializer. This means that properties must have an init or set accessor to be changed in a with-expression.

---
+++ 9.0/WithExpressions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WithExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("Mads", "Nielsen");
+            var otherPerson = person with { LastName = "Torgersen" };
+
+            Console.WriteLine($"person is {person}");
+            Console.WriteLine($"otherPerson is {otherPerson}");
+        }
+
+        public record Person(string FirstName, string LastName);
+    }
+}
