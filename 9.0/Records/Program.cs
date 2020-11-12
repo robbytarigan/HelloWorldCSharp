b2e2e4b7f16753a81dd054294e960a5c03f49abf
Robby Tarigan <robby.tarigan@cirium.com>
2020-11-12 22:12:59 +0000

    A record is still a class, but the record keyword imbues it with several additional value-like behaviors. Generally speaking, records are defined by their contents, not their identity. In this regard, records are much closer to structs, but records are still reference types.

---
+++ 9.0/Records/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Records
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person1 = new Person { FirstName = "Robby", LastName = "Tarigan" };
+            var person2 = new Person { FirstName = "Robby", LastName = "Tarigan" };
+
+            Console.WriteLine($"person1 == person2 is {person1 == person2}");
+        }
+    }
+
+    public record Person
+    {
+        public string? FirstName { get; init; }
+        public string? LastName { get; init; }
+    }
+}
