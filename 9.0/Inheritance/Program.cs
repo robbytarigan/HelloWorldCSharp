397f2ece37e3d3b94372d118fefa9749b97382ab
Robby Tarigan <robby.tarigan@cirium.com>
2020-11-12 22:29:36 +0000

    A with-expression will still copy the whole object and keep the runtime type. n the same manner, value equality makes sure the two objects have the same runtime type, and then compares all their state.

---
+++ 9.0/Inheritance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person student = new Student { FirstName = "Mads", LastName = "Nielsen", ID = 129 };
+
+            var otherStudent = student with { LastName = "Torgersen" };
+            Console.WriteLine(otherStudent is Student); // true
+
+            Person similarStudent = new Student { FirstName = "Mads", LastName = "Nielsen", ID = 130 };
+            Console.WriteLine(student != similarStudent); //true, since ID's are different
+        }
+    }
+
+    public record Person
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+    }
+
+    public record Student : Person
+    {
+        public int ID;
+    }
+}
