5d901790952a8efcacd10c0abf64a7510564b5e8
Robby Tarigan <robby.tarigan@cirium.com>
2020-11-16 22:10:45 +0000

    It’s sometimes useful to express that a method override in a derived class has a more specific return type than the declaration in the base type.

---
+++ 9.0/CovariantReturn/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CovariantReturn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Animal t = new Tiger();
+
+            Console.WriteLine(t.GetFood());
+        }
+    }
+
+    abstract class Animal
+    {
+        public abstract Food GetFood();
+    }
+
+    class Tiger : Animal
+    {
+        public override Meat GetFood() => new();
+    }
+
+    public class Food { }
+
+    public class Meat : Food {}
+}
