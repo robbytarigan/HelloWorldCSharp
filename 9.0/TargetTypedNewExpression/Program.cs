c8ba38a5ebe70ed2ed72e57eb41a88dd42e97caa
Robby Tarigan <robby.tarigan@cirium.com>
2020-11-16 22:05:09 +0000

    `new` expression can leave out the type if there’s a clear type that the expression is being assigned to.

---
+++ 9.0/TargetTypedNewExpression/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TargetTypedNewExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new (3, 5);
+
+            Point[] ps = { new (1, 2), new (5, 2), new (5, -3), new (1, -3) }; 
+
+            Console.WriteLine(p);
+            Console.WriteLine(ps);
+        }
+    }
+
+    public record Point(int X, int Y);
+}
